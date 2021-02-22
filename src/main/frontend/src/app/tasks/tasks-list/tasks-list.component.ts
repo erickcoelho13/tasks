49283b5385197e7f7448c9f9fa1bb96d920e10c1
Erick Coelho <erck.coelho@gmail.com>
2021-02-22 17:27:30 -0300

    Angular Component Layout

---
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {Task} from "src/app/tasks/task.model";
+
+@Component({
+  selector: 'app-tasks-list',
+  templateUrl: './tasks-list.component.html',
+  styleUrls: ['./tasks-list.component.css']
+})
+export class TasksListComponent implements OnInit {
+
+    tasks: Task[] = [];
+
+    constructor() { }
+
+    ngOnInit(): void {
+        this.tasks.push(new Task(1, "Task 1", true, "22/02/2020"));
+        this.tasks.push(new Task(2, "Task 2", false, "21/02/2020"));
+        this.tasks.push(new Task(3, "Task 3", false, "12/02/2020"));
+        this.tasks.push(new Task(4, "Task 4", false, "02/02/2020"));
+
+  }
+
+}
